7ad85d4d08fcbab8e5834fd410524586c724121f
ShowvenSuresh <showven1201@gmail.com>
2025-06-06 22:53:46 +0800

    changed wallpaper

--- awesome/theme.lua
+++ awesome/theme.lua
@@ -98,7 +98,7 @@ theme.titlebar_maximized_button_normal_active   = themes_path .. "default/titleb
 theme.titlebar_maximized_button_focus_active    = themes_path .. "default/titlebar/maximized_focus_active.png"
 
 theme.wallpaper                                 =
-"~/.config/awesome/wallpaper/paper1.jpg"
+"~/.config/awesome/wallpaper/sunset-moon-synthwave-night-scenery-digital-art-4k-wallpaper-uhdpaper.com-429@5@c.jpg"
 -- You can use your own layout icons like this:
 theme.layout_fairh                              = themes_path .. "default/layouts/fairhw.png"
 theme.layout_fairv                              = themes_path .. "default/layouts/fairvw.png"
